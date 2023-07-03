5f2be36c26588d7672388438f44090a4fe3b0e06
Rekreatur <78133876+Rekreatur@users.noreply.github.com>
2023-07-03 12:31:54 +0400

    Init2

---
+++ src/main/java/com/example/Annihilator/AnnihilatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Annihilator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnihilatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnihilatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Annihilator/AnnihilatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Annihilator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnihilatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
